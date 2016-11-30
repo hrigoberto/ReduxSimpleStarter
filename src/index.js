db262fca010eb1f30293bf474f4f39c3ba18b8b8
jfkbeatkid <hernadezrigoberto@gmail.com>
2016-11-29 22:02:45 -0500

    added searchbar component and eslintrc file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar'
+
+const API_KEY = 'AIzaSyBQ2w6c9aqrn1Q2BB0a5M2zI6BJvozlimQ';
 
 // Create a new componenet that creates some HTML.
 
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>  
+  );
 }
 
 // Take this component of generated HTML and show it on the page
