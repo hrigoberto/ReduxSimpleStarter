b62b04366b5bcd385a300705bc69455779980f0b
jfkbeatkid <hernadezrigoberto@gmail.com>
2016-12-06 21:41:42 -0500

    youtube iframes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar'
-
-const API_KEY = 'AIzaSyBQ2w6c9aqrn1Q2BB0a5M2zI6BJvozlimQ';
-
+import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
 // Create a new componenet that creates some HTML.
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>  
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({videos});
+    })
+  }
+
+  render(){
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 // Take this component of generated HTML and show it on the page
