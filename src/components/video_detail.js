475021b585d7646df55c29fb3897a85095c7714d
jfkbeatkid <hernadezrigoberto@gmail.com>
2016-12-10 12:14:30 -0500

    got the search populating correctly

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -6,12 +6,13 @@ const VideoDetail = ({video}) => {
   }
 
   const videoId = video.id.videoId;
-  const url = `https://www.youtube.com/embed/${videoId}`
+  const url = `https://www.youtube.com/embed/${videoId}`;
+  console.log(videoId, 'videoId in details', video, 'video in details');
 
   return (
     <div className="video-detail col-md-8">
-      <div className="embed-responsive embed-response-16by9">
-        <iframe className="embed-responsive-item" src={url}></iframe>
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe className="embed-responsive-item" src={url} ></iframe>
       </div>
       <div className="details">
         <div>{video.snippet.title}</div>
